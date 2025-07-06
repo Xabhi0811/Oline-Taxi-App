28382be1278072d835dbc821f227c64a762ca92a
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-07-06 15:32:59 +0530

    logout added in user

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -34,7 +34,7 @@ socketID:{
 })
 
 userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id},process.env.jwt_SECRET);
+const token = jwt.sign({ _id: this._id}, process.env.JWT_SECRET, { expiresIn: '24h'});
     return token;
 }
 
