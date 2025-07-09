76257f44500c0831714a67abf2d7952aea04da2f
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-07-10 01:58:30 +0530

    frontent things

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import UserLogin from './pages/UserLogin.jsx'
+import UserSignup from './pages/UserSignup.jsx'
+import CaptainLogin from './pages/CaptainLogin.jsx'
+import CaptainSignup from './pages/CaptainSignup.jsx'
+
 
 const App = () => {
   return (
-   
     <Routes>
-      <Route path="/" element={<Home />} /> 
-      
-      </Routes>   
-      
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<UserLogin />} />
+      <Route path="/Signup" element={<UserSignup />} />
+      <Route path="/capatain-login" element={<CaptainLogin />} />
+      <Route path="/capatain-Signup" element={<CaptainSignup/>} />
+    </Routes>
   )
 }
 
--- Frontend/src/Pages/UserLogin.jsx
+++ Frontend/src/Pages/UserLogin.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { use } from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const UserLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('Email:');
+  }
   return (
-    <div>
-      
+    <div className='p-7 h-screen flex flex-col justify-between'> 
+     <div> 
+      <img  className='w-16 mb-10' src="https://logospng.org/download/uber/logo-uber-4096.png" alt="" />
+      <form onSubmit={(e)=>submitHandler(e)}>
+        
+        <h3 className='text-lg font-medium mb-2'>what is your email</h3>
+       
+       
+        <input required 
+         value={email} onChange={(e)=>{
+          setEmail(e.target.value);
+         }}
+        className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' type="email" placeholder='email@example.com'/>
+        
+        
+        
+        <h3 className='text-lg font-medium mb-2'>Enter password</h3>
+
+
+         <input required 
+         value={password} onChange={(p)=>{
+          setPassword(p.target.value);
+         }}
+
+         className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' type="password" placeholder='password'/>
+         
+        <button 
+        className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'>
+          Login
+        </button>
+
+         <p className='text-center'> New here ? <Link to='/Signup' className='text-blue-600'>create new account </Link></p>
+
+      </form>
+
+      </div>
+      <div>
+        <button className='bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base' > Sign in as captain </button>
+        
+      </div>
     </div>
   )
 }
