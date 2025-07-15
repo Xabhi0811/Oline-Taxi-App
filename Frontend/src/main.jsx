b927bed26ea7926fb9f2ba46112464be5f25ec61
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-07-15 23:39:33 +0530

    captain new

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,15 +4,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from "./context/UserContext.jsx";;
-
+import { UserProvider } from "./context/UserContext.jsx";
+import  CaptainContext  from "./context/CaptainContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CaptainContext>
     <UserProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </UserProvider>
+    </CaptainContext>
   </StrictMode>
 );
